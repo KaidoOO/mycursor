dc71da9a994cf9f549106021903392bc2a151def
camilo <camilo.barottin@gmail.com>
2025-10-05 21:37:33 +0200

    add verbose

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import os
 import sys
 from dotenv import load_dotenv
 from google import genai
+from google.genai import types
 
 def main():
     print("Hello from mycursor!")
@@ -10,15 +11,23 @@ def main():
     
     client = genai.Client(api_key=api_key)
     if(len(sys.argv) > 1):
-        print(f"cli arguments {len(sys.argv)}")
-        first_prompt = sys.argv[1]
+        user_prompt = sys.argv[1]
     else:
         print("Please provide a prompt as a command line argument.")
         sys.exit(1)
-    response = client.models.generate_content(model="gemini-2.0-flash-001", contents=first_prompt)
+    
+    messages = [
+        types.Content(role="user", parts=[types.Part(text=user_prompt)]),
+        ]
+        
+    response = client.models.generate_content(model="gemini-2.0-flash-001", contents=messages)
+    if(len(sys.argv) > 2 and sys.argv[2] == "--verbose"):
+        print(f"User prompt: {user_prompt}")
+        print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+        print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+        
     print(response.text)
-    print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
-    print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+   
 
 if __name__ == "__main__":
     main()
