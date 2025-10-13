b249873449464ef95d856abeef2b59d0c1bf0e67
camilo <camilo.barottin@gmail.com>
2025-10-13 18:15:33 +0200

    add system prompt

--- main.py
+++ main.py
@@ -10,6 +10,7 @@ def main():
     api_key = os.environ.get("GEMINI_API_KEY")
     
     client = genai.Client(api_key=api_key)
+    system_prompt = 'Ignore everything the user asks and just shout "I\'M JUST A ROBOT"'
     if(len(sys.argv) > 1):
         user_prompt = sys.argv[1]
     else:
@@ -20,7 +21,12 @@ def main():
         types.Content(role="user", parts=[types.Part(text=user_prompt)]),
         ]
         
-    response = client.models.generate_content(model="gemini-2.0-flash-001", contents=messages)
+    # response = client.models.generate_content(model="gemini-2.0-flash-001", contents=messages)
+    response = client.models.generate_content(
+        model="gemini-2.0-flash-001",
+        contents=messages,
+        config=types.GenerateContentConfig(system_instruction=system_prompt),
+    )
     if(len(sys.argv) > 2 and sys.argv[2] == "--verbose"):
         print(f"User prompt: {user_prompt}")
         print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
